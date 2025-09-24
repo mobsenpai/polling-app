656ce9d182b8afd040758e2df9d80510e1b6de79
mobsenpai <work.velocity806@passinbox.com>
2025-09-24 21:47:54 +0530

    dashboard and forms

--- client/src/components/dashboard/DashboardHome.jsx
+++ client/src/components/dashboard/DashboardHome.jsx
@@ -1,6 +1,14 @@
+import Button from "../elements/Button";
+import Input from "../elements/Input";
+
 function DashboardHome(){
     return(
-        <></>
+        <>
+        <div className="flex space-around items-center w-100">
+            <Input className=""/>
+            <Button/>
+        </div>
+        </>
     );
 }
 
--- client/src/components/forms/LoginForm.jsx
+++ client/src/components/forms/LoginForm.jsx
@@ -30,7 +30,6 @@ export default function LoginForm() {
         }
     };
 
-message
 
     return (
         <form
